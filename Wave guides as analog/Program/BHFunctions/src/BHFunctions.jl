85390bd9da871a458ddc0c465c3f8d912cb04662
Ro <ronaldo.navarroa@gmail.com>
2026-09-17 16:58:29 -0600

    feat: Add program (construction)

--- Wave guides as analog/Program/BHFunctions/src/BHFunctions.jl
+++ Wave guides as analog/Program/BHFunctions/src/BHFunctions.jl
@@ -11,9 +11,9 @@ matrixofcreacreationannann,
 H_theta
 
 using LinearAlgebra #Uso de algebra lineal
-using Arpack #Encontrar eigen-valores
+#using Arpack #Encontrar eigen-valores
 using SparseArrays #Uso de sparse-arrays 
-using ExponentialUtilities #Uso de matrices sparse en exponencial en un producto con un vector   
+#using ExponentialUtilities #Uso de matrices sparse en exponencial en un producto con un vector   
 
 include("hamiltonian.jl")
 include("basis.jl")
